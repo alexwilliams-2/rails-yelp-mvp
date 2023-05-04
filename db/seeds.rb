98aaf6b4462c474769eecb3d0829b7008b46a83c
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-05-04 18:51:19 +0100

    added index route action and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+category = %w(chinese italian french japanese belgian)
+
+puts "Cleaning Database"
+
+Restaurant.destroy_all
+
+puts "creating new restaurants...populating database"
+
+5.times do
+  restaurant = Restaurant.new(name: Faker::Commerce.vendor,
+    address: Faker::Address.street_address,
+    category: category.sample)
+    restaurant.save!
+puts "This restaurant has an id of #{restaurant.id}"
+end
+
+puts "Seed complete"
